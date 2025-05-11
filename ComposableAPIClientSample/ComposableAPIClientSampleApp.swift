7cc1aa23bdce5c058c350f9fa6116845d1c1d54a
cranoo <75974473+cranoo3@users.noreply.github.com>
2025-05-11 14:31:21 +0900

    Initial Commit

---
+++ ComposableAPIClientSample/ComposableAPIClientSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ComposableAPIClientSampleApp.swift
+//  ComposableAPIClientSample
+//
+//  Created by cranoo on 2025/05/11.
+//
+
+import SwiftUI
+
+@main
+struct ComposableAPIClientSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ComposableAPIClientSampleTests/ComposableAPIClientSampleTests.swift
@@ -0,0 +1,17 @@
+//
+//  ComposableAPIClientSampleTests.swift
+//  ComposableAPIClientSampleTests
+//
+//  Created by cranoo on 2025/05/11.
+//
+
+import Testing
+@testable import ComposableAPIClientSample
+
+struct ComposableAPIClientSampleTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
