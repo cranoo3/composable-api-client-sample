fb4efe6287cbf8daa9d1ff17ad30e3a28a72680f
cranoo <75974473+cranoo3@users.noreply.github.com>
2025-05-30 00:16:53 +0900

    サンプル

---
+++ ComposableAPIClientSample/Example/ComposableAlert.swift
@@ -0,0 +1,18 @@
+//
+//  ComposableAlert.swift
+//  ComposableAPIClientSample
+//
+//  Created by cranoo on 2025/05/29.
+//
+
+import SwiftUI
+
+struct ComposableAlert: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ComposableAlert()
+}
